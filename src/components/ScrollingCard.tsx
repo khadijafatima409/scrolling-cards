ab42b9e8f422279330b482308c3e0c7f1123b8e2
khadjafatima409 <khadijafatima530@gmail.com>
2024-02-07 20:55:54 +0500

    design header and footer

--- src/components/ScrollingCard.tsx
+++ src/components/ScrollingCard.tsx
@@ -3,13 +3,13 @@ import React from "react";
 const ScrollingCard = () => {
   return( <>
 
-    <div className="bg-black-pearl text-white h-[200px] flex justify-center items-center">Header</div>
-    <div>Card1</div>
-    <div>Card2</div>
-    <div>Card3</div>
-    <div>Card4</div>
-    <div>Card5</div>
-    <div>Footer</div>
+    <div className="header">Header</div>
+    <div >Card1</div>
+    <div >Card2</div>
+    <div >Card3</div>
+    <div >Card4</div>
+    <div >Card5</div>
+    <div className="footer">Footer</div>
 
   </>)
 };
