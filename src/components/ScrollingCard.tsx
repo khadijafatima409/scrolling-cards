5abd44eb3e49d5e9992a49c64b9586752530b81a
khadjafatima409 <khadijafatima530@gmail.com>
2024-02-07 20:50:25 +0500

    design header

--- src/components/ScrollingCard.tsx
+++ src/components/ScrollingCard.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const ScrollingCard = () => {
   return( <>
 
-    <div>Header</div>
+    <div className="bg-black-pearl text-white h-[200px] flex justify-center items-center">Header</div>
     <div>Card1</div>
     <div>Card2</div>
     <div>Card3</div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,19 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Head from "next/head";
+import ScrollingCard from "@/components/ScrollingCard";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={` min-h-screen  ${inter.className}`}
     >
       <Head>
         <title>Scrolling Cards</title>
       </Head>
+      <ScrollingCard/>
     </main>
   );
 }
