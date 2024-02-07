bd1ff2d2bc3a6ef01c5e3f54c1365392c624299e
khadjafatima409 <khadijafatima530@gmail.com>
2024-02-07 20:44:31 +0500

    add prettier, structure component

--- src/components/ScrollingCard.tsx
+++ src/components/ScrollingCard.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const ScrollingCard = () => {
-  return <div>ScrollingCard</div>;
+  return( <>
+
+    <div>Header</div>
+    <div>Card1</div>
+    <div>Card2</div>
+    <div>Card3</div>
+    <div>Card4</div>
+    <div>Card5</div>
+    <div>Footer</div>
+
+  </>)
 };
 
 export default ScrollingCard;
