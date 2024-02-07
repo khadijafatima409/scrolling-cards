9ad6607c72f9eb5d0ddcd68aa06324741ac3711c
khadjafatima409 <khadijafatima530@gmail.com>
2024-02-07 21:10:27 +0500

    set filter for the image,

--- src/components/ScrollingCard.tsx
+++ src/components/ScrollingCard.tsx
@@ -8,7 +8,10 @@ const ScrollingCard = () => {
    <div  className="card">Card1</div>
     <div  className="card">
       <img src="https://source.unsplash.com/random/1000x1000" alt=""  />
-      Card2</div>
+        <div className="card-content">
+        Card2
+        </div>
+    </div>
     <div  className="card">Card3</div>
     <div className="card" >Card4</div>
     <div  className="card">Card5</div>
