d08bbf27dc326044a95d02c28f6d93c4fa37767e
khadjafatima409 <khadijafatima530@gmail.com>
2024-02-07 20:56:25 +0500

    add style for cards

--- src/components/ScrollingCard.tsx
+++ src/components/ScrollingCard.tsx
@@ -4,11 +4,11 @@ const ScrollingCard = () => {
   return( <>
 
     <div className="header">Header</div>
-    <div >Card1</div>
-    <div >Card2</div>
-    <div >Card3</div>
-    <div >Card4</div>
-    <div >Card5</div>
+    <div  className="card">Card1</div>
+    <div  className="card">Card2</div>
+    <div  className="card">Card3</div>
+    <div className="card" >Card4</div>
+    <div  className="card">Card5</div>
     <div className="footer">Footer</div>
 
   </>)
