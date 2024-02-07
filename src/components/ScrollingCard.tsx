e6cccf31e9088273430930248a08adc5f6f22833
khadjafatima409 <khadijafatima530@gmail.com>
2024-02-08 00:31:53 +0500

    struture header

--- src/components/ScrollingCard.tsx
+++ src/components/ScrollingCard.tsx
@@ -4,12 +4,25 @@ import React from "react";
 const ScrollingCard = () => {
   return( <>
 
-    <div className="header bg-black-pearl text-white h-[200px] flex justify-center items-center">Header</div>
+    <div className="header bg-black-pearl text-white h-[200px] flex justify-center items-center">
+      <div>
+        <Image src="/logo.png" alt="" height={100} width={100}/>
+      </div>
+      <ul className="flex gap-5 text-lg">
+        <li>builds</li>
+        <li>n&w</li>
+        <li>join</li>
+      </ul>
+      <button>Start</button>
+    </div>
    <div>
-   <section className=" w-full h-[100vh] bg-[url('/jon-tyson-AMXJL_MvW4I-unsplash.jpg')] bg-cover bg-center bg-no-repeat  text-white flex justify-center items-center sticky top-0">
+   <section className=" w-full h-[100vh] bg-[url('/jocke-wulcan-KLOW1bD616Y-unsplash.jpg')] bg-cover bg-center bg-no-repeat  text-white flex justify-center items-center sticky top-0">
    
         <div className="absolute h-[100%] w-[100%] left-0 top-0 z-20 flex flex-col justify-center items-center">
-        Card1
+        <div className="absolute h-[100%] w-[100%] left-0 top-0 z-20 flex flex-col justify-center items-center">
+        <h2 className="font-bold text-3xl mb-5 ">Dare to Dream</h2>
+        <p className="w-[700px] text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad laudantium, aliquam deleniti consectetur dicta id vitae unde rerum architecto repellat dolores quam obcaecati non magni nemo voluptatibus soluta vel tempore!</p>
+        </div>
         </div>
    </section>
     <section  className=" w-full h-[100vh] bg-[url('/jahanzeb-ahsan-UziXisDkH_s-unsplash.jpg')] bg-cover bg-center bg-no-repeat text-white flex justify-center items-center sticky top-0">  
