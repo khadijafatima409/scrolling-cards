c3e9eb8983ba68cf6b513221fc51d2fedb67259d
khadjafatima409 <khadijafatima530@gmail.com>
2024-02-07 21:02:33 +0500

    add position sticky for cards

--- src/components/ScrollingCard.tsx
+++ src/components/ScrollingCard.tsx
@@ -4,11 +4,13 @@ const ScrollingCard = () => {
   return( <>
 
     <div className="header">Header</div>
-    <div  className="card">Card1</div>
+   <div>
+   <div  className="card">Card1</div>
     <div  className="card">Card2</div>
     <div  className="card">Card3</div>
     <div className="card" >Card4</div>
     <div  className="card">Card5</div>
+   </div>
     <div className="footer">Footer</div>
 
   </>)
