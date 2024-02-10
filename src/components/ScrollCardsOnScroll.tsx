f5915678bbdaefc48475082fa4101fd12165dd0f
khadjafatima409 <khadijafatima530@gmail.com>
2024-02-10 20:34:56 +0500

    implement map and complete design cards witrh tailwind css

--- src/components/ScrollCardsOnScroll.tsx
+++ src/components/ScrollCardsOnScroll.tsx
@@ -2,49 +2,28 @@ import React from 'react'
 import classNames from 'classnames'
 const ScrollCardsOnScroll = () => {
   return (
-    // <div className=" container my-0 mx-auto w-[90%]">
-      
-    //   <ul id="cards " className='pl-0 list-none grid grid-cols-1 grid-rows-4 h-[87vh] gap-[4vw] pb-[4.5em] mb-[4vw]'>
-    //     <li className="card sticky top-0" id="card1">
-    //       <div className="card-body bg-[#52b2cf]">
-    //         <h2> Card 1</h2>
-    //       </div>
-    //     </li>
-    //     <li className="card" id="card2">
-    //       <div className="card-body bg-[#7EC4Cf]">
-    //         <h2>Card 2</h2>
-    //       </div>
-    //     </li>
-    //     <li className="card" id="card3">
-    //       <div className="card-body bg-[#9cadce]">
-    //         <h2>Card 3</h2>
-    //       </div>
-    //     </li>
-    //     <li className="card" id="card4">
-    //       <div className="card-body bg-[#d4afb9]">
-    //         <h2>Card 4</h2>
-    //       </div>
-    //     </li>
-    //   </ul>
-    // </div>
-    // className={classNames(
-    //   "block w-full text-text-light outline-none text-xs px-2 3xl:h-14 3xl:text-xl 3xl:px-4",
-    //   className,
-    //   {
-    //     ["bg-ghost-white !rounded-sm"]: variant === "primary",
-    //   },
-    // )}
-    <>
-        <div className='my-0 mx-auto w-[90%]'>
-           {colors.map((color, index) => (
-            <div key={index} className={`bg-[${color}] h-[87vh] w-full rounded-[50px] sticky top-4`}></div>
-             ))}
-    </div>
 
-    </>
+    <div className="container mx-auto w-[90%]">
+      <ul className="list-none grid grid-cols-1 grid-rows-4 gap-4vw pb-4">
+        {cardsData.map((card, index) => (
+          <li key={index} className="sticky top-0" style={{ paddingTop: `${card.padding}px` }}>
+            <div
+              className={`bg-[${card.bgColor}] p-7 h-[85vh] flex justify-center items-center rounded-[50px]`}
+            >
+              <h2 className="text-5xl font-bold">{card.text}</h2>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
 export default ScrollCardsOnScroll
 
-const colors = ['#52b2cf', '#7EC4Cf', '#9cadce', '#d4afb9'];
+const cardsData = [
+  { bgColor: '#52b2cf', padding: 10, text: 'Card 1' },
+  { bgColor: '#7EC4Cf', padding: 40, text: 'Card 2' },
+  { bgColor: '#9cadce', padding: 80, text: 'Card 3' },
+  { bgColor: '#d4afb9', padding: 120, text: 'Card 4' },
+];
